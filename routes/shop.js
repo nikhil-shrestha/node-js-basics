98bd1a38353f2b466045de6d99d0bfb363e7b028
illusionist <nikhil.shrestha1995@gmail.com>
2019-02-09 14:19:31 +0545

    completed pug template engine

--- routes/shop.js
+++ routes/shop.js
@@ -3,12 +3,17 @@ const path = require("path");
 const express = require("express");
 
 const rootDir = require("../utils/path");
+const adminData = require("./admin");
 
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
   // console.log('In another middleware!!');
-  res.sendFile(path.join(rootDir, "views", "shop.html"));
+  // console.log("[Shop.js]", adminData.products);
+  // res.sendFile(path.join(rootDir, "views", "shop.html"));
+  const products = adminData.products;
+
+  res.render("shop", { prods: products, pageTitle: "My Shop", path: "/" });
 });
 
 module.exports = router;
