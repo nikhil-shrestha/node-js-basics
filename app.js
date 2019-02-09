91684cc892b4427e2ba493786faeafd043a29c17
illusionist <nikhil.shrestha1995@gmail.com>
2019-02-09 14:35:33 +0545

    working with handlebars template engine

--- app.js
+++ app.js
@@ -2,24 +2,26 @@ const path = require("path");
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const expressHbs = require("express-handlebars");
 
 const app = express();
 
-app.set('view engine', 'pug');
-app.set('views', 'views');
+app.engine("hbs", expressHbs());
+app.set("view engine", "hbs");
+app.set("views", "views");
 
 const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
   // res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
-  res.render('404', {pageTitle: '404 Error'});
+  res.render("404", { pageTitle: "404 Error" });
 });
 
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -13,7 +13,7 @@ router.get("/", (req, res, next) => {
   // res.sendFile(path.join(rootDir, "views", "shop.html"));
   const products = adminData.products;
 
-  res.render("shop", { prods: products, pageTitle: "My Shop", path: "/" });
+  res.render("shop", { prods: products, pageTitle: "My Shop", path: "/", hasProducts: products.length > 0 });
 });
 
 module.exports = router;
