2c9b634b05acdc39af0124be7a3769b2a0d6d333
illusionist <nikhil.shrestha1995@gmail.com>
2019-02-09 17:55:38 +0545

    added model and added data in json

--- controllers/products.js
+++ controllers/products.js
@@ -1,4 +1,4 @@
-const products = [];
+const Product = require("../models/product");
 
 exports.getAddProduct = (req, res, next) => {
   // res.sendFile(path.join(rootDir, "views", "add-product.html"));
@@ -12,11 +12,13 @@ exports.getAddProduct = (req, res, next) => {
 };
 
 exports.postAddProduct = (req, res, next) => {
-  products.push({ title: req.body.title });
+  const product = new Product(req.body.title);
+  product.save();
   res.redirect("/");
 };
 
 exports.getProducts = (req, res, next) => {
+  Product.fetchAll(products => {
     res.render("shop", {
       prods: products,
       pageTitle: "My Shop",
@@ -25,4 +27,5 @@ exports.getProducts = (req, res, next) => {
       activeShop: true,
       productCSS: true
     });
-  }
+  });
+};
