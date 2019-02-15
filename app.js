b10606f7e339a6a60d34cd21d46e29e6f4730ca3
illusionist <nikhil.shrestha1995@gmail.com>
2019-02-15 09:02:47 +0545

    updated error handling

--- app.js
+++ app.js
@@ -43,11 +43,19 @@ app.use(csrfProtection);
 app.use(flash());
 
 app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+  next();
+});
+
+app.use((req, res, next) => {
+  // throw new Error("Sync Dummy");
   if (!req.session.user) {
     return next();
   }
   User.findById(req.session.user._id)
     .then(user => {
+      // throw new Error("dummy");
       if (!user) {
         return next();
       }
@@ -56,14 +64,8 @@ app.use((req, res, next) => {
       next();
     })
     .catch(err => {
-      throw new Error(err);
+      next(new Error(err));
     });
-});
-
-app.use((req, res, next) => {
-  res.locals.isAuthenticated = req.session.isLoggedIn;
-  res.locals.csrfToken = req.csrfToken();
-  next();
 });
 
 app.use("/admin", adminRoutes);
@@ -76,7 +78,12 @@ app.use(errorController.get404);
 
 app.use((error, req, res, next) => {
   // res.render(error.httpStatusCode).render(...)
-  res.redirect("/500");
+  // res.redirect("/500");
+  res.status(500).render("500", {
+    pageTitle: "Error!",
+    path: "/500",
+    isAuthenticated: req.session.isLoggedIn
+  });
 });
 
 mongoose
